8083faf3ea6d44c6cb3b340cb8cb820b3b2937dc
mmolent <molentmichele.kennedy@gmail.com>
2023-12-03 16:54:00 +0100

    3d commit

--- librerie/lista.hpp
+++ librerie/lista.hpp
@@ -7,15 +7,27 @@
 typedef int T;
 
 struct nodo{
-  static T val;
-  static nodo* next;
+  T val;
+  nodo* next;
+  nodo* prev;
 };
 
  struct lista {
   nodo* head;
   int dim;
+
   lista();
+  lista(T );
   ~lista();
+  void lista_print();
+  void lista_add(T val);
 };
+
+static void lista_deinit(nodo* l){
+  if(l != nullptr) {
+    lista_deinit(l -> next);
+    delete l;
+  }
+}
 
 #endif
