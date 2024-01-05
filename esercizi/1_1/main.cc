ac93904c195bf012366c706f7e5eb4582d153614
mmolent <molentmichele.kennedy@gmail.com>
2024-01-05 15:58:31 +0100

    15th commit

--- esercizi/1_1/main.cc
+++ esercizi/1_1/main.cc
@@ -4,6 +4,28 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-  if ()
-    return 0;
+  if (argc != 3) {
+    cerr << "Error usage: " << argv[0] << " <input file> <output file>" << endl;
+    exit(1);
+  }
+
+  ifstream in(argv[1]);
+  if (in.fail()) {
+    cerr << "Error opening input file " << argv[1] << endl;
+    exit(2);
+  }
+  ofstream out(argv[2]);
+  if (out.fail()) {
+    cerr << "Error opening output file" << argv[2] << endl;
+    in.close();
+    exit(3);
+  }
+  char *buffer = new char[256];
+  while (in.getline(buffer, 256)) {
+    out << buffer << endl;
+  }
+
+  in.close();
+  out.close();
+  return 0;
 }
--- librerie/albero.hpp
+++ librerie/albero.hpp
@@ -1,28 +1,29 @@
-//albero di nodi interi
+// albero di nodi interi
 
 #ifndef ALBERO_HPP
 #define ALBERO_HPP
 #include <iostream>
-typedef nodo* Albero;
-
 
 struct nodo {
   int value;
-  nodo* left;
-  nodo* right;
+  nodo *left;
+  nodo *right;
 };
+typedef nodo *Albero;
+typedef Albero albero;
 #endif
-//init_albero
+// init_albero
 Albero init_albero();
 Albero init_albero(int value);
-//deinti_albero
+// deinti_albero
 void deinit_albero(Albero &a);
-//metodi principali
+// metodi principali
 void insert(Albero &a, int value);
-//funzione che stampa l'albero in modo da vedere le identazioni ecc dalla root alla foglia
+// funzione che stampa l'albero in modo da vedere le identazioni ecc dalla root
+// alla foglia
 void print_identata(const Albero a);
 bool search(const Albero a, int value);
-nodo* search_nodo(const Albero a, int value);
+nodo *search_nodo(const Albero a, int value);
 void inorder(const Albero a);
 void preorder(const Albero a);
 void postorder(const Albero a);
